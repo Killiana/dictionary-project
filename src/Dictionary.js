9de0cf9d87f2954a3254bd35d3e11d6548faba20
Killiana <illiana9875@gmail.com>
2021-09-24 12:22:39 +0100

    Added API Integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,19 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
+
 export default function Dictionary() {
   let [key, setKey] = useState({});
+
+  function getResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function Search(event) {
     event.preventDefault();
     alert(`searching ${key}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/colour${key}`;
+    axios.get(apiUrl).then(getResponse);
   }
   function handleChange(event) {
     setKey(event.target.value);
