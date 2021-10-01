9ee279e1ef8d337505010bb50a17fc8bdd7968ec
Killiana <illiana9875@gmail.com>
2021-10-01 12:51:28 +0100

    Rendered meanings and examples

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,18 +1,22 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary() {
-  let [key, setKey] = useState({});
+  let [key, setKey] = useState("");
+  let [results, setResults] = useState(null);
 
   function getResponse(response) {
     console.log(response.data[0]);
+    setResults(response.data[0]);
+    console.log(response.data[0].meanings[0].definitions[0].definition);
   }
 
   function Search(event) {
     event.preventDefault();
-    alert(`searching ${key}`);
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/colour${key}`;
+    //alert(`searching ${key}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${key}`;
     axios.get(apiUrl).then(getResponse);
   }
   function handleChange(event) {
@@ -24,6 +28,7 @@ export default function Dictionary() {
       <form onSubmit={Search}>
         <input type="search" onChange={handleChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
